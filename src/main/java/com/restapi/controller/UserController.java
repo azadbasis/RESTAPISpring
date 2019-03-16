4b7b70f3010053dbd47eabeb71567f20e7db194d
Azhar <azadbasis@gmail.com>
2019-03-17 00:23:26 +0600

    Create user rest controller

---
+++ src/main/java/com/restapi/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.restapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") //http:localhost:8080/users/
+public class UserController  {
+}
