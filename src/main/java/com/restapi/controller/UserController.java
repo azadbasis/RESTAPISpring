a598ef45cd88b6105b166995a400720ba1ca6c3a
Azhar <azadbasis@gmail.com>
2019-03-17 01:04:39 +0600

    Reading query string request parameter

--- src/main/java/com/restapi/controller/UserController.java
+++ src/main/java/com/restapi/controller/UserController.java
@@ -9,8 +9,12 @@ public class UserController {
 
 
     @GetMapping
-    public String getUsers(@RequestParam(value = "page") int page, @RequestParam(value = "limit") int limit) {
-        return "get user was called with page = " + page + " and limit = " + limit;
+    public String getUsers(
+            @RequestParam(value = "page",defaultValue = "1") int page,
+            @RequestParam(value = "limit",defaultValue = "50") int limit,
+            @RequestParam(value = "sort",defaultValue = "desc",required = false) String sort
+    ) {
+        return "get user was called with page = " + page + " and limit = " + limit+" and sort "+sort;
     }
 
     @GetMapping(path = "/{userId}")
