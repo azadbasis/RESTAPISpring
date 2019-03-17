1f8a9882b3069f10108a5b88af94dfcba9552bb3
Azhar <azadbasis@gmail.com>
2019-03-17 21:52:01 +0600

    Validating Http Post Request Body

--- src/main/java/com/restapi/controller/UserController.java
+++ src/main/java/com/restapi/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/users")//http://localhost:8080/users
@@ -48,7 +50,7 @@ public class UserController {
                     {MediaType.APPLICATION_XML_VALUE,
                             MediaType.APPLICATION_JSON_VALUE
                     })
-    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
 
         UserRest returnValue = new UserRest();
         returnValue.setEmail(userDetails.getEmail());
--- src/main/java/com/restapi/model/UserDetailsRequestModel.java
+++ src/main/java/com/restapi/model/UserDetailsRequestModel.java
@@ -1,10 +1,25 @@
 package com.restapi.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserDetailsRequestModel {
 
+    @NotNull(message = "First name can not be null")
+    @Size(min = 2,message = "First name must not be less than  two characters")
     private String firstName;
+
+    @NotNull(message = "Last name can not be null")
+    @Size(min = 2,message = "Last name must not be less than  two characters")
     private String lastName;
+
+    @NotNull(message = "Email can not be null")
+    @Email
     private String email;
+
+    @NotNull(message = "Password can not be null")
+    @Size(min = 8, max=16,message = "Passord must be equal or greater than 8 characters and less than 16 characters")
     private String password;
 
 
