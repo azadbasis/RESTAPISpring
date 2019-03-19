77ec73897258d3bc35b5786da7cc98159be5dd59
Azhar <azadbasis@gmail.com>
2019-03-19 13:26:22 +0600

    Return custom error message Exception

--- src/main/java/com/restapi/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/restapi/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.restapi.exceptions;
 
+import com.restapi.model.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
-        return new ResponseEntity<Object>(ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if (errorMessageDescription == null)
+            errorMessageDescription = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
+        return new ResponseEntity<Object>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
